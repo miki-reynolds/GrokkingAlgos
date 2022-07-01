2db2742f40d15008e32bd7f1d45847a53fdc567d
miki-reynolds <98548191+miki-reynolds@users.noreply.github.com>
2022-07-01 02:24:01 -0500

    Longest Substring with Distinct Characters

---
+++ Sliding Window/Longest Substring with Distinct Characters (hard).py
@@ -0,0 +1,60 @@
+'''
+Given a string, find the length of the longest substring, which has all distinct characters.
+
+ Example 1:
+Input: String="aabccbb"
+Output: 3
+Explanation: The longest substring with distinct characters is "abc".
+Example 2:
+
+Input: String="abbbb"
+Output: 2
+Explanation: The longest substring with distinct characters is "ab".
+Example 3:
+
+Input: String="abccde"
+Output: 3
+Explanation: Longest substrings with distinct characters are "abc" & "cde".
+
+Solution
+
+This problem follows the Sliding Window pattern, and we can use a similar dynamic sliding window strategy as discussed in Longest Substring with K Distinct Characters. We can use a HashMap to remember the last index of each character we have processed. Whenever we get a duplicate character, we will shrink our sliding window to ensure that we always have distinct characters in the sliding window.
+Time Complexity
+The above algorithm’s time complexity will be O(N)O(N), where ‘N’ is the number of characters in the input string.
+
+Space Complexity
+The algorithm’s space complexity will be O(K)O(K), where KK is the number of distinct characters in the input string. This also means K<=NK<=N, because in the worst case, the whole string might not have any duplicate character, so the entire string will be added to the HashMap. Having said that, since we can expect a fixed set of characters in the input string (e.g., 26 for English letters), we can say that the algorithm runs in fixed space O(1)O(1); in this case, we can use a fixed-size array instead of the HashMap.
+'''
+
+
+def non_repeat_substring(string):
+    window_start, max_length = 0, 0
+    char_index_map = {}
+    # try to extend the range [windowStart, windowEnd]
+    for window_end in range(len(string)):
+        right_char = string[window_end]
+        # if the map already contains the 'right_char', shrink the window from the beginning
+        # so that we have only one occurrence of 'right_char'
+        if right_char in char_index_map:
+            # this is tricky; in the current window, we will not have any 'right_char' after
+            # its previous index and if 'window_start' is already ahead of the last index of
+            # 'right_char', we'll keep 'window_start'
+            # basically, gotta increment the window_start to the next position when encounter repeating character
+            window_start = max(window_start, char_index_map[right_char] + 1)
+
+        # insert the 'right_char' into the map when first encounter it
+        char_index_map[right_char] = window_end
+        # remember the maximum length so far
+        max_length = max(max_length, window_end - window_start + 1)
+
+    return max_length
+
+
+def main():
+    print("Length of the longest substring: " + str(non_repeat_substring("aabccbb")))
+    print("Length of the longest substring: " + str(non_repeat_substring("abbbb")))
+    print("Length of the longest substring: " + str(non_repeat_substring("abccde")))
+
+
+main()
+# 3, 2, 3
