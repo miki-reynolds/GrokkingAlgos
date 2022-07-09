c4767f15365121af933db1c81aa4679227bf8277
miki-reynolds <98548191+miki-reynolds@users.noreply.github.com>
2022-07-08 21:57:09 -0500

    Middle of the LinkedList

--- FastAndSlowPointers/Middle of the LinkedList (easy).py
+++ FastAndSlowPointers/Middle of the LinkedList (easy).py
@@ -18,6 +18,11 @@ Solution
 One brute force strategy could be to first count the number of nodes in the LinkedList and then find the middle node in the second iteration. Can we do this in one iteration?
 We can use the Fast & Slow Pointers method such that the fast pointer is always twice the nodes ahead of the slow pointer. This way, when the fast pointer reaches the end of the LinkedList, the slow pointer will be pointing at the middle node.
 
+Time Complexity
+The above algorithm will have a time complexity of O(N) where ‘N’ is the number of nodes in the LinkedList.
+
+Space Complexity
+The algorithm runs in constant space O(1).
 """
 
 
